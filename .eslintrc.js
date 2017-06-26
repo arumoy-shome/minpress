3de0d641f19621cd3d5f04ef6bda7dc7dd9be0e6
Arumoy Shome <arumoy.shome@gmail.com>
2017-06-26 15:55:44 -0400

    Fix error in eslintrc file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,15 @@
 module.exports = {
     "extends": "airbnb-base",
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true,
+        "mocha": true
+    },
     "plugins": [
         "import",
     ],
-    "rules": [
+    "rules": {
         "semi": 0
-    ]
+    }
 };
